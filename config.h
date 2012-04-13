7a9548b0a99c42f33ba62acab48c129ef9985042
Rob Pilling <rob@egbert>
2012-04-13 21:05:10 +0100

    "I" and "A"

--- config.h
+++ config.h
@@ -7,7 +7,12 @@
 MotionKey motion_keys[] = {
 	{ '\033',         m_move,       { .pos = { -1, 0 } },              UI_INSERT },
 
+	{ 'I',            m_sol,        MOTION_ARG_NONE,                   UI_NORMAL },
+
 	{ 'a',            m_move,       { .pos = { 1, 0 } },               UI_NORMAL },
+
+	{ 'A',            m_eol,        { .pos = { 1, 0 } },               UI_NORMAL },
+	{ 'A',            m_move,       { .pos = { 1, 0 } },               UI_NORMAL },
 
 	{ 'j',            m_move,       { .pos = {  0,  1 } },             UI_NORMAL },
 	{ 'k',            m_move,       { .pos = {  0, -1 } },             UI_NORMAL },
@@ -28,11 +33,17 @@ MotionKey motion_keys[] = {
 	{ 0, NULL, MOTION_ARG_NONE, 0 }
 };
 
+#define INS_KEY(k) \
+	{ k, k_set_mode, { UI_INSERT }, UI_NORMAL }
+
 Key keys[] = {
 	/* order is important */
 	{ '\033',         k_set_mode,   { UI_NORMAL },           UI_NORMAL | UI_INSERT },
-	{ 'i',            k_set_mode,   { UI_INSERT },           UI_NORMAL },
-	{ 'a',            k_set_mode,   { UI_INSERT },           UI_NORMAL },
+
+	INS_KEY('i'),
+	INS_KEY('I'),
+	INS_KEY('a'),
+	INS_KEY('A'),
 
 	{ ':',            k_cmd,        KEY_ARG_NONE,            UI_NORMAL }, /* k_set_mode instead? */
 
