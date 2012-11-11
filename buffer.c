a136fbe33ed3ab3c0b2e723e4e0bdd88bcbeb830
Rob Pilling <rob@jeffraw>
2012-11-11 22:53:07 +0000

    Show _all_ errors

--- buffer.c
+++ buffer.c
@@ -82,14 +82,8 @@ int buffer_replace_fname(buffer_t *b, const char *fname)
 	FILE *f = fopen(fname, "r");
 	int r;
 
-	if(!f){
-		if(errno == ENOENT){
-			list_free(b->head);
-			b->head = list_new();
-			return 1;
-		}
+	if(!f)
 		return 0;
-	}
 
 	r = buffer_replace_file(b, f);
 	fclose(f);
