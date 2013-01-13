27aa60b3fc898a5ec6fab7d7f7fc9b76017e0257
Rob Pilling <rob@crabcake>
2013-01-13 13:07:52 +0000

    +[0-9] as an argument specifies an offset

--- ui.c
+++ ui.c
@@ -62,7 +62,8 @@ void ui_main()
 	extern Key keys[];
 	extern MotionKey motion_keys[];
 
-	ui_redraw();
+	ui_redraw(); /* this first, to frame buf_sel */
+	ui_cur_changed(); /* this, in case there's an initial buf offset */
 
 	while(ui_running){
 		int ch = nc_getch();
