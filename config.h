2fb0e61534ee891c827798198535991269c13245
Rob Pilling <rob@egbert>
2013-03-27 21:12:57 +0000

    {} motions are linewise

--- config.h
+++ config.h
@@ -19,8 +19,8 @@ const motionkey_t motion_keys[] = {
 	{ 'h',            { m_move, { .pos = { -1,  0 } }, M_EXCLUSIVE } },
 	{ 'l',            { m_move, { .pos = {  1,  0 } }, M_EXCLUSIVE } },
 
-	{ '{',            { m_para,     { -1 }, M_NONE } },
-	{ '}',            { m_para,     { +1 }, M_NONE } },
+	{ '{',            { m_para,     { -1 }, M_LINEWISE } },
+	{ '}',            { m_para,     { +1 }, M_LINEWISE } },
 
 	{ 'f',            { m_find,     { .find_type = 0             }, M_NONE } },
 	{ 'F',            { m_find,     { .find_type = F_REV         }, M_NONE } },
