0f0774f1e863961b3c6421387ec06322abbbe15c
Rob Pilling <rob@egbert>
2014-12-14 18:41:59 +0000

    Ctrl-W, f opens a new window using the cursor-filename

--- keys.c
+++ keys.c
@@ -920,7 +920,32 @@ void word_gofile(const char *fname, bool flag)
 		return;
 	}
 
-	if(edit_common(fname, false)){
+	bool success = false;
+	if(flag){
+		buffer_t *new;
+		const char *err;
+		buffer_new_fname(&new, fname, &err);
+
+		if(err){
+			ui_err("%s: %s", fname, err);
+		}else{
+			success = true;
+		}
+
+		if(success){
+			window *wnew = window_new(new);
+
+			window_add_neighbour(windows_cur(), neighbour_up, wnew);
+			buffer_release(new);
+
+			windows_set_cur(wnew);
+		}
+
+	}else{
+		success = edit_common(fname, false);
+	}
+
+	if(success){
 		ui_redraw();
 		ui_cur_changed();
 	}
