d03804e9ec82b80b1cae083950d1c7838e921197
Rob Pilling <robpilling@gmail.com>
2015-11-18 20:12:57 +0000

    ^R in command-mode will insert strings, e.g. %, ^w, ^f

--- prompt.c
+++ prompt.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
@@ -8,6 +9,70 @@
 #include "ncurses.h"
 #include "io.h"
 #include "mem.h"
+
+/* ctrl-r */
+#include "pos.h"
+#include "region.h"
+#include "list.h"
+#include "buffer.h"
+#include "window.h"
+#include "windows.h"
+#include "buffers.h"
+
+static void prompt_ctrl_r(char **const buf, size_t *const len, size_t *const i)
+{
+	int y, x;
+	nc_get_yx(&y, &x);
+	nc_addch('"');
+	nc_set_yx(y, x);
+
+	bool wasraw;
+	int ch = io_getch(IO_NOMAP, &wasraw, /*domaps:*/true);
+
+	/* f: cursor filename
+	 * w: cursor word
+	 * W: cursor WORD
+	 * %: filename [% register]
+	 */
+	char *to_insert = NULL;
+	bool free_to_insert = true;
+
+	switch(ch){
+		case CTRL_AND('f'):
+			to_insert = window_current_fname(windows_cur());
+			break;
+		case CTRL_AND('w'):
+			to_insert = window_current_word(windows_cur());
+			break;
+		case '%':
+			to_insert = (char *)buffer_fname(buffers_cur());
+			free_to_insert = false;
+			break;
+
+		default:
+			goto cancel;
+	}
+
+	if(!to_insert)
+		goto cancel;
+
+	size_t to_insert_len = strlen(to_insert);
+
+	if(*i + to_insert_len + 1 >= *len){
+		*len = *i + to_insert_len + 1;
+		*buf = urealloc(*buf, *len);
+	}
+
+	memcpy(*buf + *i, to_insert, to_insert_len + 1);
+	*i += to_insert_len;
+
+	if(free_to_insert)
+		free(to_insert);
+	return;
+
+cancel:
+	nc_clrtoeol();
+}
 
 char *prompt(char promp, const char *initial)
 {
@@ -60,6 +125,12 @@ char *prompt(char promp, const char *initial)
 			case '\n':
 			case '\r':
 				reading = false;
+				break;
+
+			case CTRL_AND('r'):
+				prompt_ctrl_r(&buf, &len, &i);
+				nc_set_yx(nc_LINES() - 1, 1);
+				nc_addstr(buf);
 				break;
 
 			case CTRL_AND('u'):
