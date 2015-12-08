dfb6d8effb1f3f97e7a7bd8456b32c5ce6a01e12
Rob Pilling <robpilling@gmail.com>
2015-12-08 19:06:32 +0000

    Revert "TODO: fixing pulling out shellout_read_write()"
    This reverts commit a5542852e975a8b7f9e6fcbe53d4bc1dd2b2d8e9.

--- external.h
+++ external.h
@@ -5,6 +5,5 @@ void shellout_wait_ret(void);
 int shellout(const char *cmd);
 
 bool shellout_write(const char *cmd, list_t *seeked, int nlines);
-bool shellout_read_write(const char *cmd, list_t *list, int nlines, list_t **const out);
 
 #endif
